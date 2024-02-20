40c278600296ebc5493fdcf3aaa23b96ec7a5f64
AirellAristo <78353843+AirellAristo@users.noreply.github.com>
2024-02-20 22:21:22 +0700

    Delete 71200609_Pertemuan2Guided directory

--- 71200609_Pertemuan2Guided/Guided1_71200609.py
+++
@@ -1,7 +0,0 @@
-#Latihan 2.1
-
-berat = float(input("Masukkan Berat Badan (Kg): "))
-tinggi = float(input("Masukkan Tinggi Badan (m): "))
-rumusBMI = berat/tinggi**2
-
-print("Hasil Body Mass index adalah",round(rumusBMI,2))
